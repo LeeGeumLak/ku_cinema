18f6702ed204711b21ce79dbc98a58339aa2ba4b
ro-chest-47 <ro_chest_47@naver.com>
2019-05-17 17:44:23 +0900

    0.0.0

---
+++ OSSP_project/ku_cinema/src/main/java/ku_cinema/KuCinemaApplication.java
@@ -0,0 +1,13 @@
+package ku_cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuCinemaApplication.class, args);
+	}
+
+}
