533b5b461f4665b0b60170a571471260979e6f7d
ro-chest-47 <ro_chest_47@naver.com>
2019-05-26 20:03:58 +0900

    service&model&controller

--- OSSP_project/ku_cinema/src/main/java/ku_cinema/repository/customerRepo.java
+++ OSSP_project/ku_cinema/src/main/java/ku_cinema/repository/customerRepo.java
@@ -1,5 +1,13 @@
 package ku_cinema.repository;
 
-public class customerRepo {
+import ku_cinema.model.*;
+import org.springframework.data.repository.CrudRepository;
+/* 	we'll create, read, update, delete entity by CrudRepository. 	*/
+/*  reference URL : https://spring.io/guides/gs/accessing-data-jpa/ */
+//import org.springframework.data.repository.query.Param;
+
+public interface customerRepo extends CrudRepository<customer, String>{
+	
+	customer findByID(String ID);
 
 }
