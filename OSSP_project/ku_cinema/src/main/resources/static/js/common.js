8858870c629f1321b7cbbeb331d2ef9f52243daf
Yewon Jeong <yeyeah_1@daum.net>
2019-06-23 16:11:04 +0900

    상세정보 페이지

--- OSSP_project/ku_cinema/src/main/resources/static/js/common.js
+++ OSSP_project/ku_cinema/src/main/resources/static/js/common.js
@@ -2,4 +2,10 @@ $(function() {
     $(".redirect-btn").click(function(e) {
         location.href = $(e.currentTarget).children()[0].href
     })
+    
+    $(".star-rating").rateYo({
+        halfStar: true
+      });
+    var normalFill = $("#rateYo").rateYo("option", "halfStar");
+    $(".star-rating").rateYo("option", "halfStar", true);
 })
