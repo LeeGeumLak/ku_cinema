7f1b9c612f07971832d340fcf27c653329ab0716
Yewon Jeong <yeyeah_1@daum.net>
2019-06-23 18:14:19 +0900

    validation 부분 추가

--- OSSP_project/ku_cinema/src/main/resources/static/js/common.js
+++ OSSP_project/ku_cinema/src/main/resources/static/js/common.js
@@ -6,7 +6,12 @@ $(function() {
     var valid = $("#validation").text();
     
     if(valid == "true") {
-        $(".submenu").eq(0).html("<a href='#'>로그아웃</a>");
+        $(".submenu").eq(0).html("<a th:href='@{/}'>로그아웃</a>");
         $(".submenu").eq(1).html("<a href='#'>마이페이지</a>");
     }
+    
+    $(".movie__list").click(function() {
+        $(".movie__list").removeClass("back-blk");
+        $(this).addClass("back-blk");
+    })
 })
